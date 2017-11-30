5f1df2fa0ba60d3ab55e28b9df6ccecbd8135a24
varunhanumath <varungudimetla@gmail.com>
2017-11-30 16:43:25 -0500

    version 0.2.0,added load balancing memeber

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,10 @@ maintainer_email 'you@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures myhaproxy'
 long_description 'Installs/Configures myhaproxy'
-version '0.1.0'
+version '0.2.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
+
+depends 'haproxy', '~> 3.0.0'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,23 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+#
+
+
+node.default['haproxy']['members'] = [{
+	'hostname' => 'ec2-34-238-127-189.compute-1.amazonaws.com',
+	'ipaddress' => '34.238.127.189',
+	'port' => 80,
+	'ssl_port' => 80
+   },{
+
+        'hostname' => 'ec2-54-161-13-244.compute-1.amazonaws.com',
+        'ipaddress' => '54.161.13.244',
+        'port' => 80,
+        'ssl_port' => 80
+
+     }
+]
+
+
+include_recipe 'haproxy::manual'
